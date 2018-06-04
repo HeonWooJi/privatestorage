0567923dccdea0142e608114b0db5250c3b689f3
JiHeonWoo <jehwu@naver.com>
2018-06-04 14:38:46 +0900

    프로젝트 생성

---
+++ MVCModelEx/src/main/java/com/narustudio/mvcmodelex/MvcModelExApplication.java
@@ -0,0 +1,12 @@
+package com.narustudio.mvcmodelex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcModelExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcModelExApplication.class, args);
+	}
+}
